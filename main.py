10c837d05a1bbd6c462d42e20d56889b087597f1
qwer <qwerqwer@mail.ru>
2023-02-22 16:45:28 +0300

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+strin = '22 Feb 12:40:52 1 сек! рост по dogeusdt на prcnt_pre=7.24 max_price=0.09076 min_pre=0.08463 max_pre_pre=0.08555 prcnt_pre_pre=6.09 last_5_prcnt=[1.079, 0.857, 0.762, 2.578, 2.191, 2.956] last_5_from_max=[2.975, 2.292, 1.697, 0.0, 0.408, 0.474]'
+
+srr = strin.split()
+result = [srr[0] + ' ' + srr[1] + ' ' + srr[2],
+          srr[7],
+          srr[9].split('=')[-1],
+          srr[14].split('[')[-1],
+          srr[15][:-1],
+          srr[16][:-1],
+          srr[17][:-1],
+          srr[18][:-1],
+          srr[19][:-1],
+          srr[20].split('[')[-1],
+          srr[21][:-1],
+          srr[22][:-1],
+          srr[23][:-1],
+          srr[24][:-1],
+          srr[25][:-1],
+          ]
+
+print(result)
